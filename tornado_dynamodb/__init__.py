bae56e8357eb1d36a0f999710e943cf1aa5c6599
Gavin M. Roy <gavinr@aweber.com>
2015-12-28 22:04:29 -0500

    Try and fix readthedocs formatting [ci skip]

--- tornado_dynamodb/__init__.py
+++ tornado_dynamodb/__init__.py
@@ -303,19 +303,19 @@ class DynamoDB(client.AsyncAWSClient):
             about provisioned throughput consumption that is returned in the
             response:
 
-              - ``INDEXES``: The response includes the aggregate
+            - ``INDEXES``: The response includes the aggregate
                 ``ConsumedCapacity`` for the operation, together with
                 ``ConsumedCapacity`` for each table and secondary index that
                 was accessed.
 
-                .. note:: Note that some operations, such as *GetItem* and
-                          *BatchGetItem*, do not access any indexes at all. In
-                          these cases, specifying ``INDEXES`` will only return
-                          ``ConsumedCapacity`` information for table(s).
-              - ``TOTAL``: The response includes only the aggregate
+                Note that some operations, such as *GetItem* and
+                *BatchGetItem*, do not access any indexes at all. In these
+                cases, specifying ``INDEXES`` will only return
+                ``ConsumedCapacity`` information for table(s).
+            - ``TOTAL``: The response includes only the aggregate
                 ``ConsumedCapacity`` for the operation.
-              - ``NONE``: No ``ConsumedCapacity`` details are included in the
-                  response.
+            - ``NONE``: No ``ConsumedCapacity`` details are included in the
+                response.
         :rtype: dict
 
         """
